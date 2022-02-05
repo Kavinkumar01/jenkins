fe75fb987e3a51a08fb06b61086d81ae1d7e4135
Kavinkumar01 <kavinkumardev7@gmail.com>
2022-02-06 01:32:26 +0530

    added print method

--- src/main/java/com/kavin/JenkinsProject/JenkinsProjectApplication.java
+++ src/main/java/com/kavin/JenkinsProject/JenkinsProjectApplication.java
@@ -8,6 +8,11 @@ public class JenkinsProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
+		JenkinsProjectApplication.print();
+	}
+	
+	public static void print() {
+		System.out.println("The pipeline has started");
 	}
 
 }
